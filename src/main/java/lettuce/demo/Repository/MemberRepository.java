f26264a26139b16347b9718005499e873fda452e
김도균 <ehrbs4789@gmail.com>
2023-05-10 16:25:06 +0900

    memberRepository

--- src/main/java/lettuce/demo/Repository/MemberRepository.java
+++ src/main/java/lettuce/demo/Repository/MemberRepository.java
@@ -16,6 +16,4 @@ public interface MemberRepository extends CrudRepository<Member,Long> {
     public boolean existsByNickname(String nickname);
     public Optional<Member> findByNickname(String nickname);
 
-
-
 }
